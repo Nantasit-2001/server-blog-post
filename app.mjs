011b65ad802b5a639fdeb146fc2c173516ab3e31
nantasit <nantasitnsboss@gmail.com>
2025-06-02 22:18:57 +0700

    fix: character in router

--- app.mjs
+++ app.mjs
@@ -1,23 +1,23 @@
 import express from "express";
  import cors from "cors";
-import postsRouter from "./router/posts.mjs";
-import authRouter from "./router/Auth.mjs";
+import Posts from "./router/posts.mjs";
+import AuthRouter from "./router/Auth.mjs";
 import Profile from "./router/profile.mjs";
 import Notifications from "./router/Notifications.mjs";
 import Categories from "./router/category.mjs";
-import article from "./router/Article.mjs";
+import Article from "./router/Article.mjs";
 const app = express();
 const port = process.env.PORT || 4001;
 
 app.use(cors({}));
 app.use(express.json());
 
-app.use("/posts", postsRouter);
-app.use("/auth", authRouter);
+app.use("/posts", Posts);
+app.use("/auth", AuthRouter);
 app.use("/profile",Profile)
 app.use("/notifications",Notifications)
 app.use("/category",Categories)
-app.use("/Article",article)
+app.use("/Article",Article)
 
 app.get("/", (req, res) => {
   res.send("Hello TechUp!");
