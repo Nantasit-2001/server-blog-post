0a15577c1a8ac91b9c64035990afd3a535f4b1b0
nantasit <nantasitnsboss@gmail.com>
2025-06-04 14:36:03 +0700

    Revert "fix: name validateToken"

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import connectionPool from "../utils/db.mjs";
 import { validatePost } from "../middleware/validatePost.js";
-import { validateToken } from "../middleware/validateToken.js";
+import { validateToken } from "../middleware/ValidateToken.js";
 import { updateLikes_Count } from "../models/postModel.mjs";
 import { updateLike,getLike_Count,checkLikeByUser } from "../models/likeModel.mjs";
 import { getCommentByPostId,createComment } from "../models/comment.mjs";
